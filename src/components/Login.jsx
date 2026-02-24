11fc0ae4284bf458072efbc6e6ee9eb0a4e8dd39
Alexis Manosca <alexismanosca@gmail.com>
2026-02-24 23:59:02 +1300

    created a skeleton for the login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import '../styles/Login.css'
+
+function Login() {
+
+	return (
+		<div class="login">
+            <h1>lillibry</h1>
+            <form>
+                <label>Username</label>
+                <input></input>
+                <label>Password</label>
+                <input></input>
+            </form>
+		</div>
+	)
+}
+
+export default Login
