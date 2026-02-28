89028e3830a6d53f46bc7728d110f293f463bf49
Alexis Manosca <alexismanosca@gmail.com>
2026-02-28 23:54:47 +1300

    uninstalled clerk

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,23 +1,12 @@
 import Shelves from "../components/Shelves.jsx";
 import Nav from "../components/Nav.jsx";
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
 function Homepage() {
     return (
         <div className="homepage">
-            <header>
-                {/* <SignedOut>
-                    <Navigate to="/login" />
-                </SignedOut>
-                <SignedIn>
-                    <Shelves/>
-                </SignedIn> */}
-                <SignedOut>
-                    <Nav />
-                    <Shelves />
-                </SignedOut>
-            </header>
+            <Nav />
+            <Shelves />
         </div>
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,26 +5,16 @@ import App from "./App.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import Homepage from "./components/Home.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ClerkProvider
-            publishableKey={PUBLISHABLE_KEY}
-            signInUrl="/login"
-            signUpUrl="/register"
-        >
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Homepage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </BrowserRouter>
-        </ClerkProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 );
