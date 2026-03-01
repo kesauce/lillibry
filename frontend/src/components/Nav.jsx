25d56b260d36c3b409cfa1cf1970c148566f88f2
Alexis Manosca <alexismanosca@gmail.com>
2026-03-01 22:30:55 +1300

    finished designing navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,7 +3,15 @@ import "../styles/Nav.css";
 function Nav() {
     return (
         <div className="nav">
-            
+            <div className="search-wrapper">
+                <img id="search-icon" src="../src/assets/icons/search.svg" />
+                <input placeholder="Search here"></input>
+            </div>
+            <div className="icons">
+                <img id="add-icon" src="../src/assets/icons/add.svg" />
+                <img id="delete-icon" src="../src/assets/icons/delete.svg" />
+                <img id="profile-icon" src="../src/assets/icons/profile.svg" />
+            </div>
         </div>
     );
 }
