33b535cf5125689d62b435955cd94bf7415cdc35
Alexis Manosca <alexismanosca@gmail.com>
2026-03-07 14:07:07 +1300

    added skeleton for add menu

--- frontend/src/components/AddMenu.jsx
+++ frontend/src/components/AddMenu.jsx
@@ -3,7 +3,14 @@ import "../styles/Menu.css"
 function AddMenu() {
     return (
         <div className="add-menu">
-            hi
+            <nav>
+                <h1>Books</h1>
+                <div class="link-wrapper"><a>Find a book</a></div>
+                <div class="link-wrapper"><a>Add a custom</a></div>
+                <h1>Shelves</h1>
+                <div class="link-wrapper"><a>Add a shelf</a></div>
+            </nav>
+
         </div>
     );
 }
