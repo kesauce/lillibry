13621b533c57c17d3d015ac278ecbee5fb3f71d1
Alexis Manosca <alexismanosca@gmail.com>
2026-03-07 14:40:44 +1300

    fixed css for nav buttons

--- frontend/src/components/AddMenu.jsx
+++ frontend/src/components/AddMenu.jsx
@@ -1,16 +1,30 @@
 import "../styles/Menu.css"
+import { useState } from "react";
 
 function AddMenu() {
+    const [activePage, setActivePage] = useState("FindBook");
+    let pageComponent;
+
+    switch (activePage){
+        default:
+            pageComponent = (
+                <p>ho</p>
+            );
+            break;
+    };
+
+
     return (
         <div className="add-menu">
             <nav>
                 <h1>Books</h1>
-                <div class="link-wrapper"><a>Find a book</a></div>
-                <div class="link-wrapper"><a>Add a custom</a></div>
+                <div onClick={() => setActivePage("FindBook")} className={`link-wrapper ${activePage === "FindBook" ? "link-wrapper-active" : ""}`}><a>Find a book</a></div>
+                <div onClick={() => setActivePage("AddBook")} className={`link-wrapper ${activePage === "AddBook" ? "link-wrapper-active" : ""}`}><a>Add a custom</a></div>
                 <h1>Shelves</h1>
-                <div class="link-wrapper"><a>Add a shelf</a></div>
+                <div onClick={() => setActivePage("AddShelf")} className={`link-wrapper ${activePage === "AddShelf" ? "link-wrapper-active" : ""}`}><a>Add a shelf</a></div>
             </nav>
 
+            {pageComponent}
         </div>
     );
 }
