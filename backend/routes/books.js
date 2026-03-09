381ca65009f13c44e96b8936512460f9ade70f05
Alexis Manosca <alexismanosca@gmail.com>
2026-03-09 21:16:09 +1300

    error handling and formatting data for querying api

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -5,17 +5,54 @@ const router = express.Router();
 
 // Routing for finding a book using API
 router.post("/find", async (req, res) => {
-    const { query } = req.body;
-    const apiURL = "https://openlibrary.org/search.json"
+    try {
+        const { query } = req.body;
+        const apiURL = "https://openlibrary.org/search.json";
 
-    // Make a serach query at OpenLibrary.org
-    let queryURL = `${apiURL}?q=${query}`;
+        // Ensure query isn't empty or less than 3 digits
+        if (!query || query.length < 3) {
+            console.err("Search query is empty or less than 3 character");
+            return res
+                .status(400)
+                .json({ message: "Query is empty or less than 3 characters." });
+        }
 
-    const data = await fetch(queryURL);
+        // Make a serach query at OpenLibrary.org
+        let queryURL = `${apiURL}?q=${query}`;
+        const apiRes = await fetch(queryURL);
 
-    const json = await data.json();
+        // Check if the API responded
+        if (!apiRes.ok) {
+            console.err(
+                `There was a problem with querying OpenLibrary.org: ${apiRes.status}`,
+            );
+            return res
+                .status(502)
+                .json({ message: "Failed to reach OpenLibrary.org." });
+        }
 
-    res.status(200).json({"message" : json})
+        // Format the data and only return the title, author, cover, key, and publishing year
+        const data = await apiRes.json();
+        let formattedResult = [];
+
+        const limit = Math.min(data.docs.length, 100);
+        for (let i = 0; i < limit; i++) {
+            let result = {
+                key: data.docs[i].key,
+                title: data.docs[i].title,
+                author: data.docs[i].author_name,
+                coverID: data.docs[i].cover_i,
+                publishYear: data.docs[i].first_publish_year,
+            };
+
+            formattedResult.push(result);
+        }
+
+        return res.status(200).json({ result: formattedResult });
+    } catch (err) {
+        console.error(`A server error has occurred: ${err}`);
+        return res.status(500).json({"message": "An unexpected server error has occurred."});
+    }
 });
 
 export default router;
