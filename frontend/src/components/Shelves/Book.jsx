35acfd4d6fa991cd79fb2a4a4ccdf0d11c1215a2
Alexis Manosca <alexismanosca@gmail.com>
2026-07-13 13:58:37 +1200

    allow horizontal wrap for the books in shelf

--- frontend/src/components/Shelves/Book.jsx
+++ frontend/src/components/Shelves/Book.jsx
@@ -2,7 +2,7 @@ import "../../styles/Book.css";
 
 function Book({title, bookKey, cover}) {
     return (
-        <div className="flex flex-col items-center justify-end h-36 max-h-36 w-24 mx-1">
+        <div className="flex flex-col items-center justify-end h-36 max-h-36 w-24 mx-1 pb-[10px]">
             <img
                 src={cover}
                 alt={title}
